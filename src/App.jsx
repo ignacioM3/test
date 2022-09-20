cd09a54c57a222026c6371a596e75a277a3ff491
ignaciom3 <ignacio.marquez159@gmail.com>
2022-09-20 19:50:25 -0300

    sube proyecto

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+
+function App() {
+ 
+  return (
+   <main>
+     <div className="card">
+        <div className="content">
+          <h2>Paisaje</h2>
+          <a href="">Ver Detalles</a>
+        </div>
+     </div>
+     <div className="card">
+        <div className="content" >
+          <h2>Paisaje</h2>
+          <a href="">Ver Detalles</a>
+        </div>
+     </div>
+     <div className="card">
+        <div className="content" >
+          <h2>Paisaje</h2>
+          <a href="">Ver Detalles</a>
+        </div>
+     </div>
+   </main>
+  )
+}
+
+export default App
